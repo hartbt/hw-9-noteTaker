70fd8c0cb0d5ae7580dcb2daf93f57f7aaa8fa19
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-24 00:08:54 -0600

    Finalized the htmlroutes.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,2 +1,13 @@
 const router = require("express").Router()
 const path = require("path")
+
+// join notes and notes.html
+router.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+})
+
+router.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/index.js"))
+})
+
+module.exports = router
