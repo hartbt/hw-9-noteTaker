6985b90e1d9ba07b8d10d4ee9b3f48a5fc6bd702
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-24 18:37:19 -0600

    Required both of the routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express()
 // when this will be used later, it's providing the port number that anything will be hosted on
 const PORT = process.env.PORT || 3001
 
+// requiring the routes
+const apiRoute = require("./routes/apiroutes")
+const htmlRoute = require("./routes/htmlroutes")
+
 app.get("/", function(req, res){
     res.json(path.join(__dirname, "public/index.html"))
   })
