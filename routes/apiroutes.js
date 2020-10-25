0d9b771126f28c7a492dee6641f0af9f1c87b473
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-24 18:32:44 -0600

    Adding a function call to get new notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,12 @@
 const action = require("../db/action")
 const router = require("express").Router()
 
+// receiving the notes 
+router.get("/notes", function(req, res){
+    action.getNotes().then((note) => res.json(notes))
+})
+
+// to create new notes
+router.post("notes", function(req, res){
+    action.addNote(req.body).then((note) => super.json(note))
+});
