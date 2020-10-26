8af765a859e24937730a78a4d9b5c674178241e1
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-26 15:30:06 -0600

    Adding action.js

---
+++ db/action.js
@@ -0,0 +1,50 @@
+const fs = require("fs")
+const util = require("util")
+const uuid = require("uuid/v1")
+
+const writeFileAsync = util.promisify(fs.writeFile)
+const readFileAsync = util.promisify(fs.readFile)
+
+class Store {
+    read(){
+        return readFileAsync("db/db.json", "utf8")
+    }
+    write(note){
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
+    getNotes(){
+        return this.read().then((notes) => {
+            let parsedNotes 
+            try{
+                parsedNotes = [].concat(JSON.parse(notes))
+            } catch(err){
+                parsedNotes = []
+            } 
+            return parsedNotes
+            
+        })
+    }
+    addNote(note){
+        const {
+            text, title
+        } = note
+        if(!text||!title){
+            throw("Please enter a note")
+        }
+        const newNote = {
+            text, id, title: uuidv1()
+        } 
+        return this.getNotes()
+        .then((notes) => [...notes, newNote]
+        )
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote)
+    }
+    removeNote(id){
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes))
+    }
+}
+
+module.exports = new Store()
