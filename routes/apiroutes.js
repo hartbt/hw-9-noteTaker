3253b627e9c01c0a8bc1fe15908f56cb457cd862
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-26 16:06:29 -0600

    Added .catch

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,16 +4,19 @@ const router = require("express").Router()
 // receiving the notes 
 router.get("/notes", function(req, res){
     action.getNotes().then((note) => res.json(notes))
+    .catch((err) => res.status(500).json(err))
 })
 
 // to create new notes
 router.post("/notes/api", function(req, res){
     action.addNote(req.body).then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err))
 });
 
 // to delete notes
 router.delete("/notes/:id", function(req, res){
     action.deleteNotes(req.params.id).then(() => res.json({ ok : true}))
+    .catch((err) => res.status(500).json(err))
 })
 
 module.exports = router 
