dbbd7b8ba278ea8f67d31db32bfafd91cb437531
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-26 15:51:56 -0600

    Removed .js added .html

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -7,7 +7,7 @@ router.get("/notes", function(req, res){
 })
 
 router.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.js"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
 module.exports = router
